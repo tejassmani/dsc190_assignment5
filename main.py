d721edba56fad865a034f41743691bc2230ad587
tejassmani <tmani@ucsd.edu>
2026-05-06 18:37:46 -0700

    Fix lint errors

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import os        # F401: imported but unused
-import sys       # F401: another unused import
+x = 1 + 1
 
-x=1+1            # E225: missing whitespace around operator
 
 def foo():
-    y = 1
-    return       # F821 / general: defined but never used (y)
+    return
 
-l = 1            # E741: ambiguous variable name 'l'
+
+l_value = 1
